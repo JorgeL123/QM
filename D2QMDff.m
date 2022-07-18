9ac24e60710faa594216dc578da5d31b323b9e05
YourPhysicsSimulator <87150821+JorgeL123@users.noreply.github.com>
2022-07-19 00:20:17 +0200

    Add files via upload

---
+++ D2QMDff.m
@@ -0,0 +1,57 @@
+clc;
+clear;
+close all;
+dx=0.1; dt=0.01; hbar=1;m=10; a=10;
+x=[-1.5*a:dx:1.5*a]; y=[-1.5*a:dx:1.5*a]; [XX,YY]=meshgrid(x,y); t=[0:dt:200];c=2;
+P=(2/pi)^(1/2)*exp(-(XX).^2-(YY+a/3).^2).*exp(j*c*YY); T=[0:0.25:200]; n=1; alfa=[0:pi/40:2*pi];
+%Define potential
+V=((XX.^2+YY.^2).^2/25-XX.^2-YY.^2)/35+0.25; ga=sqrt((1/35+sqrt(1/(35^2)-4*1/25*1/35*(0.25-1)))/(2*1/25*1/35));
+r=hbar*c/(2*m)*dt/dx;
+%%
+figure(1);
+pause(1.5)
+for k=1:length(t);
+ % Draw Wave Function for certain values of time
+if t(k)==T(n);
+%plot3(reshape(XX,length(x).^2,1),reshape(YY,length(x).^2,1),reshape(V,length(x).^2,1),'-k','LineWidth',.001)
+plot3(-y,zeros(length(x),1),(x.^4/25-x.^2)/35+0.25,'-k','LineWidth',1.3)
+hold on
+hold on
+plot3(zeros(length(x),1),x,(x.^4/25-x.^2)/35+0.25,'-k','LineWidth',1.3)
+hold on
+plot3(ga*cos(alfa),ga*sin(alfa),ones(length(alfa),1),'-k','LineWidth',1.3)
+surf(XX,YY,abs(P),'FaceAlpha',0.9)
+colormap jet
+shading interp
+axis([-a a -a a 0 1])
+xlabel('x')
+ylabel('y')
+zlabel('Ψ')
+%legend('V(x)','|Ψ(x,t)|','Re\{Ψ(x,t)\}')
+set(gca,'Ytick',[]) 
+set(gca,'Xtick',[])
+set(gca,'Ztick',[])
+F(n)=getframe(gcf);
+n=n+1;
+hold off
+end
+% RK4
+%P(1,:)=0; P(end,:)=0;P(:,1)=0; P(:,end)=0;
+K1=SchroE2(P,V,dx,hbar,m);
+K2=SchroE2(P+dt*K1/2,V,dx,hbar,m);
+K3=SchroE2(P+dt*K2/2,V,dx,hbar,m);
+K4=SchroE2(P+dt*K3,V,dx,hbar,m);
+%A=P(end); B=P(end-1);
+%C=P(1); D=P(2);
+P=P+dt/6*(K1+2*K2+2*K3+K4);
+% Mur Boundary condition: not very clean for big wave packets.
+%P(end)=B+(r-1)/(r+1)*(P(end-1)-A);
+%P(1)=D+(r-1)/(r+1)*(P(2)-C);
+end
+%%
+video=VideoWriter('QM2Dff');
+video.Quality=100;
+video.FrameRate=24;
+open(video);
+writeVideo(video,F);
+close(video);
---
+++ SchroE2.m
@@ -0,0 +1,18 @@
+function dP=SchroE2(P,V,dx,hbar,m);
+dP=zeros(length(P(1,:)));
+dP(1,:)=0; dP(end,:)=0;dP(:,1)=0; dP(:,end)=0;
+%
+for i=2:length(P(1,:))-2;
+dP(2,i)=j*hbar/(2*m)*(P(1,i)-4*P(2,i)+P(2,i+1)+P(2,i-1))/(dx^2)-j*V(2,i)*P(2,i)/hbar;
+dP(end-1,i)=j*hbar/(2*m)*(P(end,i)-4*P(end-1,i)+P(end-2,i)+P(end-1,i+1)+P(end-1,i-1))/(dx^2)-j*V(end-1,i)*P(end-1,i)/hbar;
+dP(i,2)=j*hbar/(2*m)*(P(i,1)-4*P(i,2)+P(i+1,2)+P(i-1,2))/(dx^2)-j*V(i,2)*P(i,2)/hbar;
+dP(i,end-1)=j*hbar/(2*m)*(P(i,end)-4*P(i,end-1)+P(i,end-2)+P(i+1,end-1)+P(i-1,end-1))/(dx^2)-j*V(i,end-1)*P(i,end-1)/hbar;
+end
+%
+for i=3:length(P(1,:))-2;
+    for k=3:length(P(1,:))-2;
+    dP(i,k)=j*hbar/(2*m)*(-P(i+2,k)+16*P(i+1,k)-60*P(i,k)+16*P(i-1,k)-P(i-2,k)-P(i,k+2)+16*P(i,k+1)+16*P(i,k-1)-P(i,k-2))/(12*dx^2)-j*V(i,k)*P(i,k)/hbar;
+    end
+end
+
+end
